e8663c2d9f6476e0dd2d333954ba09a9c626b857
Marc Link Cladera <117278436+linkcla@users.noreply.github.com>
2026-05-06 17:53:09 +0200

    feat(contract): Implementar la creació d'organitzacions (#405)
    * feat(contract): Implementar la creació d'organitzacions amb validacions i emmagatzematge
    
    * style(contract): Reformat amb ruff per poder realitzar pr

--- voting-contract/projects/demochain/smart_contracts/demochain/contract.py
+++ voting-contract/projects/demochain/smart_contracts/demochain/contract.py
@@ -1,8 +1,48 @@
-from algopy import ARC4Contract, String
+from algopy import ARC4Contract, arc4, BoxMap, Txn, urange, op
 from algopy.arc4 import abimethod
 
 
+class Organization(arc4.Struct):
+    name: arc4.String
+    description: arc4.String
+    admin: arc4.Address
+
+
 class Demochain(ARC4Contract):
+    def __init__(self) -> None:
+        self.org_id = arc4.UInt64(0)
+        self.organizations = BoxMap(arc4.UInt64, Organization, key_prefix="org_")
+        self.organization_names = BoxMap(arc4.String, arc4.Bool, key_prefix="on_")
+        self.census = BoxMap(
+            arc4.Tuple[arc4.UInt64, arc4.Address], arc4.Bool, key_prefix="cen_"
+        )
+
     @abimethod()
-    def hello(self, name: String) -> String:
-        return "Hello, " + name
+    def create_org(self, name: arc4.String, description: arc4.String) -> arc4.UInt64:
+        """Method for creating an organization"""
+        assert not self._is_blank(name), "org.empty-name"
+        assert not self._is_blank(description), "org.empty-description"
+        assert name not in self.organization_names, "org.already-exists"
+
+        self.org_id = arc4.UInt64(self.org_id.native + 1)
+
+        self.organizations[self.org_id] = Organization(
+            name,
+            description,
+            arc4.Address(Txn.sender),
+        )
+        self.organization_names[name] = arc4.Bool(True)
+        self.census[arc4.Tuple((self.org_id, arc4.Address(Txn.sender)))] = arc4.Bool(
+            True
+        )
+
+        return self.org_id
+
+    def _is_blank(self, s: arc4.String) -> bool:
+        b = s.native.bytes
+        if b.length == 0:
+            return True
+        for i in urange(b.length):
+            if op.getbyte(b, i) != 32:
+                return False
+        return True
